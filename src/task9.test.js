c4d16078052e0ab85d176c651ec30882158b4753
Alecto <fomenkoandrey1978@gmail.com>
2024-02-07 23:48:00 +0200

    test9 updated

--- src/task9.test.js
+++ src/task9.test.js
@@ -7,7 +7,15 @@ describe('Task #9: Transform text case', () => {
     expect(global.upperStr).toBe('SOME STRING')
   })
 
+  test('Code contains toUpperCase() method', () => {
+    expect(code).toMatch(/\.toUpperCase\(\)/)
+  })
+
   test('lowerStr is correctly transformed to lowercase', () => {
     expect(global.lowerStr).toBe('some string')
+  })
+
+  test('Code contains toLowerCase() method', () => {
+    expect(code).toMatch(/\.toLowerCase\(\)/)
   })
 })
