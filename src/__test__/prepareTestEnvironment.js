127a59d5100d18ef80e2c1bec82367c96c8eacec
Alecto <fomenkoandrey1978@gmail.com>
2024-02-12 15:08:58 +0200

    тести переміщені до папки __test__

--- src/__test__/prepareTestEnvironment.js
+++ src/__test__/prepareTestEnvironment.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-const mainJsContent = fs.readFileSync(path.join(__dirname, 'main.js'), 'utf8')
+const mainJsContent = fs.readFileSync(path.join(__dirname, '../main.js'), 'utf8')
 const modifiedContent = mainJsContent
   .replace(/\b(var|let|const)\b/g, 'global.') // Заміна var, let, const на global.
   .replace(/console\.log\(/g, '// console.log(')
